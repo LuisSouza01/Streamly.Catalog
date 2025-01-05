2c7c5e3137e526d6acb1a1de6705e370b88acdb1
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-05 15:19:26 -0300

    test: add Category instantiation test with valid data

--- tests/Streamly.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/Streamly.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -3,5 +3,37 @@ namespace Streamly.Catalog.UnitTests.Domain.Entity.Category;
 [Collection(nameof(CategoryTestFixture))]
 public class CategoryTest(CategoryTestFixture fixture)
 {
-    private readonly CategoryTestFixture _fixture = fixture;
+    [Fact(DisplayName = nameof(ShouldInstantiateCorrectly))]
+    [Trait("Domain", "Category - Aggregates")]
+    public void ShouldInstantiateCorrectly()
+    {
+        #region Arrange
+
+            var exampleCategory = fixture.GetExampleCategory();
+
+        #endregion
+
+        #region Act
+        
+            var dateTimeBefore = DateTime.UtcNow;
+            var category = new Category(
+                exampleCategory.Name, 
+                exampleCategory.Description
+            );
+            var dateTimeAfter = DateTime.UtcNow;
+
+        #endregion
+
+        #region Assert
+
+            category.Should().NotBeNull();
+            category.Id.Should().NotBeEmpty();
+            category.Name.Should().Be(exampleCategory.Name);
+            category.Description.Should().Be(exampleCategory.Description);
+            category.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+            category.CreatedAt.Should().BeOnOrAfter(dateTimeBefore).And.BeOnOrBefore(dateTimeAfter);
+            category.IsActive.Should().BeTrue();
+
+        #endregion
+    }
 }
