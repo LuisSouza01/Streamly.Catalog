41da255e81dadf7cf84b3dab8660ce81080f983f
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-05 15:43:29 -0300

    test: add test to validate Category instantiation with random isActive value

--- tests/Streamly.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/Streamly.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -39,4 +39,40 @@ public class CategoryTest(CategoryTestFixture fixture)
 
         #endregion
     }
+
+    [Theory(DisplayName = nameof(ShouldInstantiateWithIsActiveCorrectly))]
+    [Trait("Domain", "Category - Aggregates")]
+    [MemberData(nameof(CategoryTestDataGenerator.GetRandomBoolean), parameters: 10, MemberType = typeof(CategoryTestDataGenerator))]
+    public void ShouldInstantiateWithIsActiveCorrectly(bool isActive)
+    {
+        #region Arrange
+
+            var exampleCategory = fixture.GetExampleCategory();
+
+        #endregion
+
+        #region Act
+
+            var dateTimeBefore = DateTime.UtcNow;
+            var category = new DomainEntity.Category(
+                exampleCategory.Name, 
+                exampleCategory.Description,
+                isActive
+            );
+            var dateTimeAfter = DateTime.UtcNow;
+
+        #endregion
+
+        #region Assert
+
+            category.Should().NotBeNull();
+            category.Id.Should().NotBeEmpty();
+            category.Name.Should().Be(exampleCategory.Name);
+            category.Description.Should().Be(exampleCategory.Description);
+            category.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+            category.CreatedAt.Should().BeOnOrAfter(dateTimeBefore).And.BeOnOrBefore(dateTimeAfter);
+            category.IsActive.Should().Be(isActive);
+
+        #endregion
+    }
 }
