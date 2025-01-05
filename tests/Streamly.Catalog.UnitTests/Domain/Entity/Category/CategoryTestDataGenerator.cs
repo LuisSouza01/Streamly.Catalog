5813e4b6045e2b17e193d77c2ccc364a0d2b497b
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-05 15:39:45 -0300

    feat: add GetRandomBoolean method for random boolean generation

--- tests/Streamly.Catalog.UnitTests/Domain/Entity/Category/CategoryTestDataGenerator.cs
+++ tests/Streamly.Catalog.UnitTests/Domain/Entity/Category/CategoryTestDataGenerator.cs
@@ -1,6 +1,16 @@
 namespace Streamly.Catalog.UnitTests.Domain.Entity.Category;
 
-public class CategoryTestDataGenerator
+public static class CategoryTestDataGenerator
 {
-    
+    public static IEnumerable<object[]> GetRandomBoolean(int numberOfTests)
+    {
+        var random = new Random();
+
+        for (var i = 0; i < numberOfTests; i++)
+        {
+            var isActive = random.Next(0, 10) < 5;
+
+            yield return new object[] { isActive };
+        }
+    }
 }
