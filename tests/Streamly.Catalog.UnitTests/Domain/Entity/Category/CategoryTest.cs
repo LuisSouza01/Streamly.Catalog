22cf734cae1985c1a0578ef9c1babf6ff7bd095e
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-05 15:16:24 -0300

    chore: inject CategoryTestFixture into CategoryTest class

--- tests/Streamly.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/Streamly.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -1,6 +1,7 @@
 namespace Streamly.Catalog.UnitTests.Domain.Entity.Category;
 
-public class CategoryTest
+[Collection(nameof(CategoryTestFixture))]
+public class CategoryTest(CategoryTestFixture fixture)
 {
-    
+    private readonly CategoryTestFixture _fixture = fixture;
 }
