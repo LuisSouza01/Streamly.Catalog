4a5611e1c5cfb245fddeb2d9b0acc1cd9f4976c5
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-05 15:25:34 -0300

    created Category entity using primary constructor and properties

--- src/Streamly.Catalog.Domain/Entities/Category.cs
+++ src/Streamly.Catalog.Domain/Entities/Category.cs
@@ -1,6 +1,12 @@
 namespace Streamly.Catalog.Domain.Entities;
 
-public class Category
+public class Category(
+    string name, 
+    string description, 
+    bool isActive = true)
 {
-    
+    public string Name { get; } = name;
+    public string Description { get; } = description;
+    public bool IsActive { get; } = isActive;
+    public DateTime CreatedAt { get; } = DateTime.UtcNow;
 }
