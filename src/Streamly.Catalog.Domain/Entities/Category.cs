557f91d7f24ab1e14a92574164efa99bf01c5305
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-05 15:57:39 -0300

    feat: add validation to Category entity when name is null, empty, or whitespace

--- src/Streamly.Catalog.Domain/Entities/Category.cs
+++ src/Streamly.Catalog.Domain/Entities/Category.cs
@@ -1,14 +1,28 @@
+using Streamly.Catalog.Domain.Exceptions;
 using Streamly.Catalog.Domain.SeedWork;
 
 namespace Streamly.Catalog.Domain.Entities;
 
-public class Category(
-    string name, 
-    string description, 
-    bool isActive = true) : AggregateRoot
+public class Category : AggregateRoot
 {
-    public string Name { get; } = name;
-    public string Description { get; } = description;
-    public bool IsActive { get; } = isActive;
-    public DateTime CreatedAt { get; } = DateTime.UtcNow;
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public bool IsActive { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    
+    public Category(string name, string description, bool isActive = true) : base()
+    {
+        Name = name;
+        Description = description;
+        IsActive = isActive;
+        CreatedAt = DateTime.UtcNow;
+        
+        Validate();
+    }
+
+    private void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(Name))
+            throw new EntityValidationException("Name should not be empty or null");
+    }
 }
