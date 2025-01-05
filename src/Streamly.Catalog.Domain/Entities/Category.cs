979647070c81c6e7c0cb7aad0809d13244d04f52
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-05 15:28:48 -0300

    feat: inherit Category class from AggregateRoot

--- src/Streamly.Catalog.Domain/Entities/Category.cs
+++ src/Streamly.Catalog.Domain/Entities/Category.cs
@@ -1,9 +1,11 @@
+using Streamly.Catalog.Domain.SeedWork;
+
 namespace Streamly.Catalog.Domain.Entities;
 
 public class Category(
     string name, 
     string description, 
-    bool isActive = true)
+    bool isActive = true) : AggregateRoot
 {
     public string Name { get; } = name;
     public string Description { get; } = description;
