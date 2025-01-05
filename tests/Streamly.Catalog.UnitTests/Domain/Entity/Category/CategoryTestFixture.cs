25a1bf9927fedf0ceb5cd4958c41009976c7db27
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-05 15:11:32 -0300

    chore: implement CategoryTestFixture to generate example Category data

--- tests/Streamly.Catalog.UnitTests/Domain/Entity/Category/CategoryTestFixture.cs
+++ tests/Streamly.Catalog.UnitTests/Domain/Entity/Category/CategoryTestFixture.cs
@@ -1,6 +1,12 @@
+using Streamly.Catalog.UnitTests.Common;
+
 namespace Streamly.Catalog.UnitTests.Domain.Entity.Category;
 
-public class CategoryTestFixture
+public class CategoryTestFixture : BaseFixture
 {
-    
+    public Category GetExampleCategory()
+        => new(
+            Faker.Commerce.ProductName(),
+            Faker.Commerce.ProductDescription()
+        );
 }
