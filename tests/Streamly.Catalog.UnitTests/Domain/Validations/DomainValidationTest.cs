98a6be53f2e44be7dc027cc27096940baab9efd3
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-06 08:27:55 -0300

    test: create ShouldThrowWhenValueIsNull to validate null values

--- tests/Streamly.Catalog.UnitTests/Domain/Validations/DomainValidationTest.cs
+++ tests/Streamly.Catalog.UnitTests/Domain/Validations/DomainValidationTest.cs
@@ -1,7 +1,33 @@
+using Streamly.Catalog.Domain.Exceptions;
+
 namespace Streamly.Catalog.UnitTests.Domain.Validations;
 
 [Collection(nameof(DomainValidationTestFixture))]
 public class DomainValidationTest(DomainValidationTestFixture fixture)
 {
-    
+    [Fact(DisplayName = nameof(ShouldThrowWhenValueIsNull))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void ShouldThrowWhenValueIsNull()
+    {
+        #region Arrange
+
+            string? value = null;
+            var fieldName = fixture.GetExampleFieldName();
+
+        #endregion
+
+        #region Act
+
+            var action =
+                () => DomainValidation.NotNull(value, fieldName);
+
+        #endregion
+
+        #region Assert
+
+            action.Should().Throw<EntityValidationException>()
+                .WithMessage($"{fieldName} should not be null.");
+
+        #endregion
+    }
 }
