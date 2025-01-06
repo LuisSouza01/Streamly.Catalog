6ec927efa4ef4cfe047b698d19b3dd139a5406d4
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-06 08:22:46 -0300

    chore: adjust DomainValidationTestFixture with BaseFixture inheritance and collection definition

--- tests/Streamly.Catalog.UnitTests/Domain/Validations/DomainValidationTestFixture.cs
+++ tests/Streamly.Catalog.UnitTests/Domain/Validations/DomainValidationTestFixture.cs
@@ -1,6 +1,12 @@
+using Streamly.Catalog.UnitTests.Common;
+
 namespace Streamly.Catalog.UnitTests.Domain.Validations;
 
-public class DomainValidationTestFixture
+public class DomainValidationTestFixture : BaseFixture
 {
     
 }
+
+[CollectionDefinition(nameof(DomainValidationTestFixture))]
+public class DomainValidationTestFixtureCollection
+    : ICollectionFixture<DomainValidationTestFixture> {}
