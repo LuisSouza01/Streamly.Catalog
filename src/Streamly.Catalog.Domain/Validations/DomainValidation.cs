b01f7f9dbb0b43c4f11959b90b69c8543e2fcaa2
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-06 08:36:50 -0300

    feat: add NotNullOrEmpty validation function to DomainValidation

--- src/Streamly.Catalog.Domain/Validations/DomainValidation.cs
+++ src/Streamly.Catalog.Domain/Validations/DomainValidation.cs
@@ -9,4 +9,10 @@ public static class DomainValidation
         if (target is null)
             throw new EntityValidationException($"{fieldName} should not be null.");
     }
+    
+    public static void NotNullOrEmpty(string? target, string fieldName)
+    {
+        if (string.IsNullOrWhiteSpace(target))
+            throw new EntityValidationException($"{fieldName} should not be empty or null.");
+    }
 }
