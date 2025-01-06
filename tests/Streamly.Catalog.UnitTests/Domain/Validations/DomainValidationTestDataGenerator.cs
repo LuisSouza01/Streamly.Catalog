7150549ed83d91b5ccb8d9763bd389229c385ecf
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-06 08:34:14 -0300

    test: create GetInvalidValues method in DomainValidationTestDataGenerator for invalid value cases

--- tests/Streamly.Catalog.UnitTests/Domain/Validations/DomainValidationTestDataGenerator.cs
+++ tests/Streamly.Catalog.UnitTests/Domain/Validations/DomainValidationTestDataGenerator.cs
@@ -1,6 +1,21 @@
 namespace Streamly.Catalog.UnitTests.Domain.Validations;
 
-public class DomainValidationTestDataGenerator
+public static class DomainValidationTestDataGenerator
 {
-    
+    public static IEnumerable<object[]> GetInvalidValues(int numberOfTests)
+    {
+        var invalidValues = new[]
+        {
+            string.Empty,
+            null,
+            "  "
+        };
+        
+        for (var i = 0; i < numberOfTests; i++)
+        {
+            var invalidValue = invalidValues[i % invalidValues.Length];
+
+            yield return new object[] { invalidValue! };
+        }
+    }
 }
