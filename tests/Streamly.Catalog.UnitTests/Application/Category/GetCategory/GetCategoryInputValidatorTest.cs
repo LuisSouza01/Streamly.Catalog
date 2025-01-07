e3f089f0b60dc2a09861745d88f42b38b7203da2
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-07 18:05:14 -0300

    test: add ShouldThrowWhenEmptyGuid to validate empty Guid input

--- tests/Streamly.Catalog.UnitTests/Application/Category/GetCategory/GetCategoryInputValidatorTest.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/GetCategory/GetCategoryInputValidatorTest.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using Streamly.Catalog.Application.UseCases.Category.GetCategory;
 
 namespace Streamly.Catalog.UnitTests.Application.Category.GetCategory;
@@ -27,6 +28,34 @@ public class GetCategoryInputValidatorTest
             validationResult.Should().NotBeNull();
             validationResult.IsValid.Should().BeTrue();
             validationResult.Errors.Should().HaveCount(0);
+
+        #endregion
+    }
+    
+    [Fact(DisplayName = nameof(ShouldThrowWhenEmptyGuid))]
+    [Trait("Application", "GetCategoryInputValidation - UseCases")]
+    public void ShouldThrowWhenEmptyGuid()
+    {
+        #region Arrange
+
+            var invalidInput = new GetCategoryInput(Guid.Empty);
+                    
+            var validator = new GetCategoryInputValidator();
+            
+        #endregion
+
+        #region Act
+
+            var validationResult = validator.Validate(invalidInput);
+
+        #endregion
+
+        #region Assert
+
+            validationResult.Should().NotBeNull();
+            validationResult.IsValid.Should().BeFalse();
+            validationResult.Errors.Should().HaveCount(1);
+            validationResult.Errors[0].ErrorMessage.Should().Be("'Id' must not be empty.");
 
         #endregion
     }
