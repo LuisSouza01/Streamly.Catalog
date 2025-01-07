52bf713ccf29d3abb3278393375bef92857bcaf8
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-07 17:40:05 -0300

    test: implemented GetInvalidInputs in CreateCategoryTestDataGenerator for throw tests

--- tests/Streamly.Catalog.UnitTests/Application/Category/CreateCategory/CreateCategoryTestDataGenerator.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/CreateCategory/CreateCategoryTestDataGenerator.cs
@@ -1,6 +1,62 @@
 namespace Streamly.Catalog.UnitTests.Application.Category.CreateCategory;
 
-public class CreateCategoryTestDataGenerator
+public static class CreateCategoryTestDataGenerator
 {
-    
+    public static IEnumerable<object[]> GetInvalidInputs(int numberOfTests)
+    {
+        var fixture = new CreateCategoryTestFixture();
+        
+        var invalidInputsList = new List<object[]>();
+        
+        const int totalInvalidCases = 5;
+
+        for (var i = 0; i < numberOfTests; i++)
+        {
+            switch (i % totalInvalidCases)
+            {
+                case 0:
+                    invalidInputsList.Add(
+                        new object[] {
+                            fixture.GetInvalidCategoryInputNameNull(),
+                            "Name should not be empty or null."
+                        }
+                    );
+                    break;
+                case 1:
+                    invalidInputsList.Add(
+                        new object[] {
+                            fixture.GetInvalidCategoryInputShortName(), 
+                            "Name should be at least 3 characters long."
+                        }
+                    );
+                    break;
+                case 2:
+                    invalidInputsList.Add(
+                        new object[] {
+                            fixture.GetInvalidCategoryInputTooLongName(), 
+                            "Name should be less or equal 255 characters long."
+                        }
+                    );
+                    break;
+                case 3:
+                    invalidInputsList.Add(
+                        new object[] {
+                            fixture.GetInvalidCategoryInputDescriptionNull(), 
+                            "Description should not be null."
+                        }
+                    );
+                    break;
+                case 4:
+                    invalidInputsList.Add(
+                        new object[] {
+                            fixture.GetInvalidCategoryInputTooLongDescription(), 
+                            "Description should be less or equal 10000 characters long."
+                        }
+                    );
+                    break;
+            }
+        }
+        
+        return invalidInputsList;
+    }
 }
