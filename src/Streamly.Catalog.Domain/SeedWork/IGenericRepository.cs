af7b0d5868bab5b81132a812eb1f9b986edcf496
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-07 17:22:02 -0300

    feat: implement IGenericRepository in domain seedwork

---
+++ src/Streamly.Catalog.Domain/SeedWork/IGenericRepository.cs
@@ -0,0 +1,9 @@
+namespace Streamly.Catalog.Domain.SeedWork;
+
+public interface IGenericRepository<TAggregate> : IRepository where TAggregate : AggregateRoot
+{
+    public Task InsertAsync(TAggregate aggregate, CancellationToken cancellationToken);
+    public Task<TAggregate> GetAsync(Guid id, CancellationToken cancellationToken);
+    public Task UpdateAsync(TAggregate aggregate, CancellationToken cancellationToken);
+    public Task DeleteAsync(TAggregate aggregate, CancellationToken cancellationToken);
+}
