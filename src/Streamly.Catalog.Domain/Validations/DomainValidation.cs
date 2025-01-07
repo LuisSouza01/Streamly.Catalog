4028428787ff559d3eda2b2eace30da2e20c7b0f
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-06 21:32:50 -0300

    feat: added MinLength validation in DomainValidation

--- src/Streamly.Catalog.Domain/Validations/DomainValidation.cs
+++ src/Streamly.Catalog.Domain/Validations/DomainValidation.cs
@@ -15,4 +15,10 @@ public static class DomainValidation
         if (string.IsNullOrWhiteSpace(target))
             throw new EntityValidationException($"{fieldName} should not be empty or null.");
     }
+    
+    public static void MinLength(string target, int minLength, string fieldName)
+    {
+        if (target.Length < minLength)
+            throw new EntityValidationException($"{fieldName} should be at least {minLength} characters long.");
+    }
 }
