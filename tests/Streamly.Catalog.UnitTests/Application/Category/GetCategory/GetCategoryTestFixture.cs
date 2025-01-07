e41792c53b20042bcc5191b00129a87bbc6dd93a
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-07 17:45:19 -0300

    chore: set up GetCategoryTestFixture structure

--- tests/Streamly.Catalog.UnitTests/Application/Category/GetCategory/GetCategoryTestFixture.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/GetCategory/GetCategoryTestFixture.cs
@@ -1,6 +1,12 @@
+using Streamly.Catalog.UnitTests.Common;
+
 namespace Streamly.Catalog.UnitTests.Application.Category.GetCategory;
 
-public class GetCategoryTestFixture
+public class GetCategoryTestFixture : BaseFixture
 {
     
 }
+
+[CollectionDefinition(nameof(GetCategoryTestFixture))]
+public class GetCategoryTestFixtureCollection
+    : ICollectionFixture<GetCategoryTestFixture> { }
