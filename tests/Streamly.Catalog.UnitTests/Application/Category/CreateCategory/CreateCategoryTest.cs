c261fa22054bb45ff47c4bdc144da6be781b35fc
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-07 17:20:01 -0300

    test: add ShouldCreateCategory unit test

--- tests/Streamly.Catalog.UnitTests/Application/Category/CreateCategory/CreateCategoryTest.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/CreateCategory/CreateCategoryTest.cs
@@ -1,7 +1,60 @@
+using Moq;
+using FluentAssertions;
+using DomainEntity = Streamly.Catalog.Domain.Entities;
+
 namespace Streamly.Catalog.UnitTests.Application.Category.CreateCategory;
 
 [Collection(nameof(CreateCategoryTestFixture))]
 public class CreateCategoryTest(CreateCategoryTestFixture fixture)
 {
-    
+    [Fact(DisplayName = nameof(ShouldCreateCategory))]
+    [Trait("Application", "Category - UseCases")]
+    public async Task ShouldCreateCategory()
+    {
+        #region Arrange
+
+            var unitOfWorkMock = fixture.GetUnitOfWorkMock();
+            var repositoryMock = fixture.GetCategoryRepositoryMock();
+            
+            var useCase = new UseCases.CreateCategory(
+                unitOfWorkMock.Object,
+                repositoryMock.Object
+            );
+            
+            var input = fixture.GetValidCategoryInput();
+
+        #endregion
+
+        #region Act
+
+            var output = await useCase.Handle(input, CancellationToken.None);
+
+        #endregion
+
+        #region Assert
+
+            repositoryMock.Verify(
+                repository => repository.InsertAsync(
+                    It.IsAny<DomainEntity.Category>(),
+                    It.IsAny<CancellationToken>()
+                ),
+                Times.Once
+            );
+            
+            unitOfWorkMock.Verify(
+                uow => uow.CommitAsync(
+                   It.IsAny<CancellationToken>() 
+                ),
+                Times.Once
+            );
+            
+            output.Should().NotBeNull();
+            output.Id.Should().NotBeEmpty();
+            output.Name.Should().Be(input.Name);
+            output.Description.Should().Be(input.Description);
+            output.IsActive.Should().Be(input.IsActive);
+            output.CreatedAt.Should().NotBeSameDateAs(default);
+
+        #endregion
+    }
 }
