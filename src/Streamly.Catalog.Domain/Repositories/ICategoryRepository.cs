b298cb3cef92245d4cfb2de4a0e727a9af984891
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-07 17:23:02 -0300

    feat: add ICategoryRepository to domain repositories

---
+++ src/Streamly.Catalog.Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,7 @@
+using Streamly.Catalog.Domain.Entities;
+using Streamly.Catalog.Domain.SeedWork;
+
+namespace Streamly.Catalog.Domain.Repositories;
+
+public interface ICategoryRepository 
+    : IGenericRepository<Category> { }
