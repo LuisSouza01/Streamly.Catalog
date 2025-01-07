3a8a4359b59788a71e44e28bfb3502395329c675
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-07 17:49:10 -0300

    feat: add example category generation methods in GetCategoryTestFixture

--- tests/Streamly.Catalog.UnitTests/Application/Category/GetCategory/GetCategoryTestFixture.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/GetCategory/GetCategoryTestFixture.cs
@@ -1,10 +1,42 @@
 using Streamly.Catalog.UnitTests.Common;
+using DomainEntity = Streamly.Catalog.Domain.Entities;
 
 namespace Streamly.Catalog.UnitTests.Application.Category.GetCategory;
 
 public class GetCategoryTestFixture : BaseFixture
 {
+    public DomainEntity.Category GetExampleCategory()
+        => new(
+            GetValidName(),
+            GetValidDescription(),
+            GetRandomBoolean()
+        );
     
+    public string GetValidName()
+    {
+        var categoryName = string.Empty;
+
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+
+        return categoryName;
+    }
+
+    public string GetValidDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+
+        return categoryDescription;
+    }
+
+    private bool GetRandomBoolean()
+        => new Random().NextDouble() < 0.5;
 }
 
 [CollectionDefinition(nameof(GetCategoryTestFixture))]
