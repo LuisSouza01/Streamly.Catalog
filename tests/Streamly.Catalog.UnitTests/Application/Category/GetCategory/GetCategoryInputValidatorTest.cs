74f61558374b5b61d21917be96cd8a38fa785f92
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-07 18:01:48 -0300

    test: implement ShouldValidate test in GetCategoryInputValidatorTest

--- tests/Streamly.Catalog.UnitTests/Application/Category/GetCategory/GetCategoryInputValidatorTest.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/GetCategory/GetCategoryInputValidatorTest.cs
@@ -1,6 +1,33 @@
+using Streamly.Catalog.Application.UseCases.Category.GetCategory;
+
 namespace Streamly.Catalog.UnitTests.Application.Category.GetCategory;
 
 public class GetCategoryInputValidatorTest
 {
-    
+    [Fact(DisplayName = nameof(ShouldValidate))]
+    [Trait("Application", "GetCategoryInputValidation - UseCases")]
+    public void ShouldValidate()
+    {
+        #region Arrange
+
+            var validInput = new GetCategoryInput(Guid.NewGuid());
+                
+            var validator = new GetCategoryInputValidator();
+            
+        #endregion
+
+        #region Act
+
+            var validationResult = validator.Validate(validInput);
+
+        #endregion
+
+        #region Assert
+
+            validationResult.Should().NotBeNull();
+            validationResult.IsValid.Should().BeTrue();
+            validationResult.Errors.Should().HaveCount(0);
+
+        #endregion
+    }
 }
