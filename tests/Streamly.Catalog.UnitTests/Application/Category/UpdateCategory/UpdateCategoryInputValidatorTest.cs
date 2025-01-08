b1fbbfb1bb6f311a30a6fc84071a33c6046808b1
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-08 17:40:33 -0300

    test: created ShouldValidate in UpdateCategoryInputValidatorTest

--- tests/Streamly.Catalog.UnitTests/Application/Category/UpdateCategory/UpdateCategoryInputValidatorTest.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/UpdateCategory/UpdateCategoryInputValidatorTest.cs
@@ -4,5 +4,30 @@ namespace Streamly.Catalog.UnitTests.Application.Category.UpdateCategory;
 public class UpdateCategoryInputValidatorTest(
     UpdateCategoryTestFixture fixture)
 {
-    
+    [Fact(DisplayName = nameof(ShouldValidate))]
+    [Trait("Application", "UpdateCategoryInputValidator - UseCases")]
+    public async Task ShouldValidate()
+    {
+        #region Arrange
+
+            var validInput = fixture.GetValidInput();
+                    
+            var validator = new UpdateCategoryInputValidator();
+            
+        #endregion
+
+        #region Act
+
+            var validationResult = validator.Validate(validInput);
+
+        #endregion
+
+        #region Assert
+
+            validationResult.Should().NotBeNull();
+            validationResult.IsValid.Should().BeTrue();
+            validationResult.Errors.Should().HaveCount(0);
+
+        #endregion
+    }
 }
