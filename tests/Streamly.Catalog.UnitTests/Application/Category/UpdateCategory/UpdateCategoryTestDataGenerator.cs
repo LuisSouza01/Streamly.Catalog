5046ab42b51a44f45fee0e30da17cbe78e2b8d51
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-08 17:32:47 -0300

    test: added GetInvalidInputs function to UpdateCategoryTestDataGenerator

--- tests/Streamly.Catalog.UnitTests/Application/Category/UpdateCategory/UpdateCategoryTestDataGenerator.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/UpdateCategory/UpdateCategoryTestDataGenerator.cs
@@ -15,4 +15,46 @@ public static class UpdateCategoryTestDataGenerator
             yield return new object[] { exampleCategory, input };
         }
     }
+    
+    public static IEnumerable<object[]> GetInvalidInputs(int numberOfTests)
+    {
+        var fixture = new UpdateCategoryTestFixture();
+        
+        var invalidInputsList = new List<object[]>();
+        
+        const int totalInvalidCases = 3;
+
+        for (var i = 0; i < numberOfTests; i++)
+        {
+            switch (i % totalInvalidCases)
+            {
+                case 0:
+                    invalidInputsList.Add(
+                        new object[] {
+                            fixture.GetInvalidCategoryInputShortName(), 
+                            "Name should be at least 3 characters long."
+                        }
+                    );
+                    break;
+                case 1:
+                    invalidInputsList.Add(
+                        new object[] {
+                            fixture.GetInvalidCategoryInputTooLongName(), 
+                            "Name should be less or equal 255 characters long."
+                        }
+                    );
+                    return invalidInputsList;
+                case 2:
+                    invalidInputsList.Add(
+                        new object[] {
+                            fixture.GetInvalidCategoryInputDescriptionNull(), 
+                            "Description should not be null."
+                        }
+                    );
+                    break;
+            }
+        }
+        
+        return invalidInputsList;
+    }
 }
