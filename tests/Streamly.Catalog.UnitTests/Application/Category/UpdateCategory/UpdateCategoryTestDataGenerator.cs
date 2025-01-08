9733040680987c7ff158971ed7ad01ffbc4fdbb9
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-08 17:14:00 -0300

    test(UpdateCategory): add GetCategoriesToUpdate method to UpdateCategoryTestDataGenerator

--- tests/Streamly.Catalog.UnitTests/Application/Category/UpdateCategory/UpdateCategoryTestDataGenerator.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/UpdateCategory/UpdateCategoryTestDataGenerator.cs
@@ -1,6 +1,18 @@
 namespace Streamly.Catalog.UnitTests.Application.Category.UpdateCategory;
 
-public class UpdateCategoryTestDataGenerator
+public static class UpdateCategoryTestDataGenerator
 {
-    
+    public static IEnumerable<object[]> GetCategoriesToUpdate(int numberOfTests = 5)
+    {
+        var fixture = new UpdateCategoryTestFixture();
+        
+        for (var i = 0; i < numberOfTests; i++)
+        {
+            var exampleCategory = fixture.GetExampleCategory();
+
+            var input = fixture.GetValidInput(exampleCategory.Id);
+
+            yield return new object[] { exampleCategory, input };
+        }
+    }
 }
