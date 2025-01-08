1a61c5f22dab55e325645c38e8f24b1d2dad19c2
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-08 18:02:58 -0300

    test: refactor CategoryTestFixture to ensure valid data by using GetValidName and GetValidDescription in GetExampleCategory

--- tests/Streamly.Catalog.UnitTests/Domain/Entities/Category/CategoryTestFixture.cs
+++ tests/Streamly.Catalog.UnitTests/Domain/Entities/Category/CategoryTestFixture.cs
@@ -7,9 +7,32 @@ public class CategoryTestFixture : BaseFixture
 {
     public DomainEntity.Category GetExampleCategory()
         => new(
-            Faker.Commerce.ProductName(),
-            Faker.Commerce.ProductDescription()
+            GetValidName(),
+            GetValidDescription()
         );
+    
+    private string GetValidName()
+    {
+        var categoryName = string.Empty;
+
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+
+        return categoryName;
+    }
+
+    private string GetValidDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+
+        return categoryDescription;
+    }
 }
 
 [CollectionDefinition(nameof(CategoryTestFixture))]
