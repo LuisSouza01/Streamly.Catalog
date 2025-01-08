4f0a86116b4b21da8e1c6be38d1e42afd93897be
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-08 17:08:38 -0300

    test(UpdateCategory): add GetValidInput function to UpdateCategoryTestFixture

--- tests/Streamly.Catalog.UnitTests/Application/Category/UpdateCategory/UpdateCategoryTestFixture.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/UpdateCategory/UpdateCategoryTestFixture.cs
@@ -1,6 +1,44 @@
+using Streamly.Catalog.UnitTests.Common;
+
 namespace Streamly.Catalog.UnitTests.Application.Category.UpdateCategory;
 
-public class UpdateCategoryTestFixture
+public class UpdateCategoryTestFixture : BaseFixture
 {
+    public UpdateCategoryInput GetValidInput(Guid? id = null)
+        => new(
+            id ?? Guid.NewGuid(),
+            GetValidName(),
+            GetValidDescription(),
+            GetRandomBoolean()
+        );
     
+    private string GetValidName()
+    {
+        var categoryName = string.Empty;
+
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+
+        return categoryName;
+    }
+
+    private string GetValidDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+
+        return categoryDescription;
+    }
+
+    private bool GetRandomBoolean()
+        => new Random().NextDouble() < 0.5;
 }
+
+[CollectionDefinition(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTestFixtureCollection
+    : UpdateCategoryTestFixture {}
