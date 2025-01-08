acb99f86c7c5ffdf0635bd592cfab82dd6e021a3
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-08 17:49:56 -0300

    test: implemented ShouldDeleteCategory test

--- tests/Streamly.Catalog.UnitTests/Application/Category/DeleteCategory/DeleteCategoryTest.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/DeleteCategory/DeleteCategoryTest.cs
@@ -1,7 +1,64 @@
+using Moq;
+
 namespace Streamly.Catalog.UnitTests.Application.Category.DeleteCategory;
 
 [Collection(nameof(DeleteCategoryTestFixture))]
 public class DeleteCategoryTest(DeleteCategoryTestFixture fixture)
 {
-    
+    [Fact(DisplayName = nameof(ShouldDeleteCategory))]
+    [Trait("Application", "DeleteCategory - UseCases")]
+    public async Task ShouldDeleteCategory()
+    {
+        #region Arrange
+
+            var repositoryMock = fixture.GetCategoryRepositoryMock();
+            var unitOfWorkMock = fixture.GetUnitOfWorkMock();
+            var validCategory = fixture.GetExampleCategory();
+
+            repositoryMock.Setup(x => x.GetAsync(
+                validCategory.Id,
+                It.IsAny<CancellationToken>()
+            )).ReturnsAsync(validCategory);
+                
+            var input = new UseCases.DeleteCategoryInput(validCategory.Id);
+            var useCase = new UseCases.DeleteCategory(
+                repositoryMock.Object,
+                unitOfWorkMock.Object
+            );
+
+        #endregion
+
+        #region Act
+
+            await useCase.Handle(input, CancellationToken.None);
+
+        #endregion
+
+        #region Assert
+        
+            repositoryMock.Verify(
+                repository => repository.GetAsync(
+                    validCategory.Id,
+                    It.IsAny<CancellationToken>()
+                ), 
+                Times.Once
+            );
+
+            repositoryMock.Verify(
+                repository => repository.DeleteAsync(
+                    validCategory,
+                    It.IsAny<CancellationToken>()
+                ),
+                Times.Once
+            );
+                
+            unitOfWorkMock.Verify(
+                repository => repository.CommitAsync(
+                    It.IsAny<CancellationToken>()
+                ),
+                Times.Once
+            );
+
+        #endregion
+    }
 }
