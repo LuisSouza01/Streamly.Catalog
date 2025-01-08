5924d7bc56da5ce95ceb317ab7b9d2a2a2b1b5b6
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-08 17:57:38 -0300

    test: implemented ShouldThrowWhenCategoryNotFound in DeleteCategoryTest

--- tests/Streamly.Catalog.UnitTests/Application/Category/DeleteCategory/DeleteCategoryTest.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/DeleteCategory/DeleteCategoryTest.cs
@@ -1,4 +1,6 @@
+using FluentAssertions;
 using Moq;
+using Streamly.Catalog.Application.Exceptions;
 using UseCases = Streamly.Catalog.Application.UseCases.Category.DeleteCategory;
 
 namespace Streamly.Catalog.UnitTests.Application.Category.DeleteCategory;
@@ -57,6 +59,55 @@ public class DeleteCategoryTest(DeleteCategoryTestFixture fixture)
                 repository => repository.CommitAsync(
                     It.IsAny<CancellationToken>()
                 ),
+                Times.Once
+            );
+
+        #endregion
+    }
+    
+    [Fact(DisplayName = nameof(ShouldThrowWhenCategoryNotFound))]
+    [Trait("Application", "DeleteCategory - UseCases")]
+    public async Task ShouldThrowWhenCategoryNotFound()
+    {
+        #region Arrange
+
+            var repositoryMock = fixture.GetCategoryRepositoryMock();
+            
+            var unitOfWorkMock = fixture.GetUnitOfWorkMock();
+
+            var exampleGuid = Guid.NewGuid();
+
+            repositoryMock.Setup(x => x.GetAsync(
+                exampleGuid,
+                It.IsAny<CancellationToken>()
+            )).ThrowsAsync(new NotFoundException($"Category '{exampleGuid}' not found."));
+                    
+            var input = new UseCases.DeleteCategoryInput(exampleGuid);
+            
+            var useCase = new UseCases.DeleteCategory(
+                repositoryMock.Object,
+                unitOfWorkMock.Object
+            );
+
+        #endregion
+
+        #region Act
+
+            var task =
+                async () => await useCase.Handle(input, CancellationToken.None);
+
+        #endregion
+
+        #region Assert
+
+            await task.Should().ThrowAsync<NotFoundException>()
+                .WithMessage($"Category '{exampleGuid}' not found.");
+        
+            repositoryMock.Verify(
+                repository => repository.GetAsync(
+                    exampleGuid,
+                    It.IsAny<CancellationToken>()
+                ), 
                 Times.Once
             );
 
