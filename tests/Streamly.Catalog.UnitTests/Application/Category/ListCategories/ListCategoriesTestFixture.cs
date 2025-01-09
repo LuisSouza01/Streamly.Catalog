2abbbb30c090c6669e3ad72a8fc3de8917923cdf
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-09 17:15:20 -0300

    test: add helper functions to ListCategoriesTestFixture

--- tests/Streamly.Catalog.UnitTests/Application/Category/ListCategories/ListCategoriesTestFixture.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/ListCategories/ListCategoriesTestFixture.cs
@@ -1,10 +1,35 @@
+using DomainEntity = Streamly.Catalog.Domain.Entities;
 using Streamly.Catalog.UnitTests.Application.Category.Common;
 
 namespace Streamly.Catalog.UnitTests.Application.Category.ListCategories;
 
 public class ListCategoriesTestFixture : CategoryUseCasesBaseFixture
 {
-    
+    public List<DomainEntity.Category> GetExampleCategoriesList(int length = 10)
+    {
+        var list = new List<DomainEntity.Category>();
+
+        for (var i = 0; i < length; i++)
+        {
+            list.Add(GetExampleCategory());
+        }
+
+        return list;
+    }
+
+    public ListCategoriesInput GetExampleInput()
+    {
+        var random = new Random();
+        
+        return new UseCases.ListCategoriesInput(
+            page: random.Next(1, 10),
+            perPage: random.Next(15, 100),
+            search: Faker.Commerce.ProductName(),
+            sort: Faker.Commerce.ProductName(),
+            dir: random.Next(0, 10) > 5 ?
+                SearchOrder.Asc : SearchOrder.Desc
+        );
+    }
 }
 
 [CollectionDefinition(nameof(ListCategoriesTestFixture))]
