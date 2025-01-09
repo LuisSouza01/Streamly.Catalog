76b999f8cc38019b8490dde55e44ae4aa740d86e
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-09 17:36:14 -0300

    test: add GetInputsWithoutAllParameters to ListCategoriesTestDataGenerator

--- tests/Streamly.Catalog.UnitTests/Application/Category/ListCategories/ListCategoriesTestDataGenerator.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/ListCategories/ListCategoriesTestDataGenerator.cs
@@ -1,6 +1,62 @@
+using Streamly.Catalog.Application.UseCases.Category.ListCategories;
+
 namespace Streamly.Catalog.UnitTests.Application.Category.ListCategories;
 
-public class ListCategoriesTestDataGenerator
+public static class ListCategoriesTestDataGenerator
 {
-    
+    public static IEnumerable<object[]> GetInputsWithoutAllParameters(int numberOfTests)
+    {
+        var fixture = new ListCategoriesTestFixture();
+
+        var inputExample = fixture.GetExampleInput();
+
+        for (var i = 0; i < numberOfTests; i++)
+        {
+            yield return (i % 5) switch
+            {
+                0 => new object[]
+                {
+                    new ListCategoriesInput()
+                },
+                1 => new object[]
+                {
+                    new ListCategoriesInput(
+                        page: inputExample.Page
+                    )
+                },
+                2 => new object[]
+                {
+                    new ListCategoriesInput(
+                        page: inputExample.Page, 
+                        perPage: inputExample.PerPage
+                    )
+                },
+                3 => new object[]
+                {
+                    new ListCategoriesInput(
+                        page: inputExample.Page, 
+                        perPage: inputExample.PerPage,
+                        search: inputExample.Search
+                    )
+                },
+                4 => new object[]
+                {
+                    new ListCategoriesInput(
+                        page: inputExample.Page, 
+                        perPage: inputExample.PerPage,
+                        search: inputExample.Search, 
+                        sort: inputExample.Sort
+                    )
+                },
+                5 => new object[]
+                {
+                    inputExample
+                },
+                _ => new object[]
+                {
+                    new ListCategoriesInput()
+                }
+            };
+        }
+    }
 }
