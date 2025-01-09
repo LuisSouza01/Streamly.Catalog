c7c11b480618f18f1d1a6be0193ddcd8443f69b1
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-09 17:37:39 -0300

    test: add ShouldListCorrectlyWithInputWithoutAnyParameters

--- tests/Streamly.Catalog.UnitTests/Application/Category/ListCategories/ListCategoriesTest.cs
+++ tests/Streamly.Catalog.UnitTests/Application/Category/ListCategories/ListCategoriesTest.cs
@@ -87,4 +87,85 @@ public class ListCategoriesTest(ListCategoriesTestFixture fixture)
 
         #endregion
     }
+    
+    [Theory(DisplayName = nameof(ShouldListCorrectlyWithInputWithoutAnyParameters))]
+    [Trait("Application", "ListCategories - UseCases")]
+    [MemberData(
+        nameof(ListCategoriesTestDataGenerator.GetInputsWithoutAllParameters), 
+        parameters: 14, 
+        MemberType = typeof(ListCategoriesTestDataGenerator)
+    )]
+    public async Task ShouldListCorrectlyWithInputWithoutAnyParameters(UseCases.ListCategoriesInput input)
+    {
+        #region Arrange
+
+            var repositoryMock = fixture.GetCategoryRepositoryMock();
+            
+            var categoriesExampleList = fixture.GetExampleCategoriesList();
+            
+            var outputRepositorySearch = new SearchOutput<DomainEntity.Category>(
+                currentPage: input.Page,
+                perPage: input.PerPage,
+                items: categoriesExampleList,
+                total: new Random().Next(50, 200)
+            );
+            
+            repositoryMock.Setup(repository => 
+                repository.SearchAsync(
+                    It.Is<SearchInput>(
+                        searchInput => searchInput.Page == input.Page
+                        && searchInput.PerPage == input.PerPage
+                        && searchInput.OrderBy == input.Sort
+                        && searchInput.Order == input.Dir
+                    ),
+                    It.IsAny<CancellationToken>()
+                )
+            ).ReturnsAsync(outputRepositorySearch);
+
+            var useCase = new UseCases.ListCategories(
+                repositoryMock.Object
+            );
+
+        #endregion
+
+        #region Act
+
+            var output = await useCase.Handle(input, CancellationToken.None);
+
+        #endregion
+
+        #region Assert
+
+            output.Should().NotBeNull();
+            output.Page.Should().Be(outputRepositorySearch.CurrentPage);
+            output.PerPage.Should().Be(outputRepositorySearch.PerPage);
+            output.Total.Should().Be(outputRepositorySearch.Total);
+            output.Items.Should().HaveCount(outputRepositorySearch.Items.Count);
+            ((List<CategoryModelOutput>)output.Items).ForEach(outputItem =>
+            {
+                var repositoryCategory = outputRepositorySearch.Items.FirstOrDefault(x => x.Id == outputItem.Id);
+                    
+                outputItem.Should().NotBeNull();
+                outputItem.Id.Should().Be(repositoryCategory!.Id);
+                outputItem.Name.Should().Be(repositoryCategory.Name);
+                outputItem.Description.Should().Be(repositoryCategory.Description);
+                outputItem.IsActive.Should().Be(repositoryCategory.IsActive);
+                outputItem.CreatedAt.Should().Be(repositoryCategory.CreatedAt);
+            });
+                
+            repositoryMock.Verify(repository => 
+                    repository.SearchAsync(
+                        It.Is<SearchInput>(
+                            searchInput => searchInput.Page == input.Page
+                                           && searchInput.PerPage == input.PerPage
+                                           && searchInput.OrderBy == input.Sort
+                                           && searchInput.Order == input.Dir
+                        ),
+                        It.IsAny<CancellationToken>()
+                    ),
+                Times.Once
+            );
+
+        #endregion
+    }
 }
