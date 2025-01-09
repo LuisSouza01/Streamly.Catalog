9104a5e2a79530e8fc4f88d862c09df3d0d02749
LuisSouza01 <luis.f.souza@outlook.com>
2025-01-09 17:27:45 -0300

    feat: add ISearchableRepository, SearchInput, and SearchOutput in seedwork domain

---
+++ src/Streamly.Catalog.Domain/SeedWork/SearchableRepository/ISearchableRepository.cs
@@ -0,0 +1,9 @@
+namespace Streamly.Catalog.Domain.SeedWork.SearchableRepository;
+
+public interface ISearchableRepository<TAggregate> where TAggregate : AggregateRoot
+{
+    Task<SearchOutput<TAggregate>> SearchAsync(
+        SearchInput input,
+        CancellationToken cancellationToken
+    );
+}
---
+++ src/Streamly.Catalog.Domain/SeedWork/SearchableRepository/SearchInput.cs
@@ -0,0 +1,15 @@
+namespace Streamly.Catalog.Domain.SeedWork.SearchableRepository;
+
+public class SearchInput(
+    int page,
+    int perPage,
+    string search,
+    string orderBy,
+    SearchOrder order)
+{
+    public int Page { get; set; } = page;
+    public int PerPage { get; set; } = perPage;
+    public string Search { get; set; } = search;
+    public string OrderBy { get; set; } = orderBy;
+    public SearchOrder Order { get; set; } = order;
+}
